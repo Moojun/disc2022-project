4bfa19ff2ad6dbb7e38cd31a30269058367be505
Moojun <mgim32137@gmail.com>
2022-09-21 15:43:34 +0900

    login 부분에서 id, password 입력 검증 기능 추가

--- js/login.js
+++ js/login.js
@@ -1,13 +1,19 @@
 // let loginBtn = document.querySelector(".submit-btn");
 // loginBtn.addEventListener("click", signup);
 
+let idText = document.querySelector('#id');
+let pwText = document.querySelector('#pw');
+
 function signup() {
   alert("정말로 가입하시겠습니까?");
-  console.log("test");
-
-  document.querySelector(".login-form").style.display = "none";
-  document.querySelector(".login-text").style.display = "none";
-  document.querySelector("#result-id").innerHTML = document.form1.id.value;
-  document.querySelector(".result").setAttribute("style","display: block;" );
+  if (idText != null && pwText != null) {
+    document.querySelector(".login-form").style.display = "none";
+    document.querySelector(".login-text").style.display = "none";
+    document.querySelector("#result-id").innerHTML = document.form1.id.value;
+    document.querySelector(".result").setAttribute("style","display: block;" );
+  }
+  else {
+    alert("아이디와 비밀번호를 정확하게 입력해 주세요");
+  }
 
 }
