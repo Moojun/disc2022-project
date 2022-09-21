f8c1e65bbc1f31dc73878648f21e8ca8591b2166
Moojun <mgim32137@gmail.com>
2022-09-21 16:50:51 +0900

    - safari에서 더 잘 표현하기 위해, alert 창 대신 confirm 창으로 변경 - 게스트 입장 버튼 활성화

--- js/login.js
+++ js/login.js
@@ -7,14 +7,17 @@
 function signup() {
   let idText = document.querySelector('#id').value;
   let pwText = document.querySelector('#pw').value;
-  alert("정말로 가입하시겠습니까?");
+  let state = confirm("정말로 가입하시겠습니까?");
 
-  if ((idText != "" )&& (pwText != "" )) {
+  if ((idText != "" )&& (pwText != "" ) && (state == true)) {
     console.log(idText);
     document.querySelector(".login-form").style.display = "none";
     document.querySelector(".login-text").style.display = "none";
     document.querySelector("#result-id").innerHTML = document.form1.id.value;
     document.querySelector(".result").setAttribute("style","display: block;" );
+  }
+  else if (state == false) {
+    return;
   }
   else {
     alert("아이디와 비밀번호를 정확하게 입력해 주세요");
