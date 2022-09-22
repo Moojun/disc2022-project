6cd61a2b06689fc2b950068999de7d023368c110
Moojun <mgim32137@gmail.com>
2022-09-22 16:22:15 +0900

    회원가입 기능과 로그인 기능 연동

--- js/login.js
+++ js/login.js
@@ -4,23 +4,36 @@
 // let idText = document.form1.id.value;
 // let pwText = document.fomr1.pw.value;
 
-function signup() {
+console.log("Current id is " , Cookies.get('id'));
+console.log("Current pw is " ,  Cookies.get('password'));
+console.log("Current email is " , Cookies.get('email'));
+// console.log("Current gender is " , Cookies.get('gender'));
+
+
+function login() {
   let idText = document.querySelector('#id').value;
   let pwText = document.querySelector('#pw').value;
-  let state = confirm("정말로 가입하시겠습니까?");
+  let state = confirm("로그인하시겠습니까?");
 
-  if ((idText != "" )&& (pwText != "" ) && (state == true)) {
-    console.log(idText);
+  if ((idText == Cookies.get('id') )&& (pwText == Cookies.get('password') ) && (state == true)) {
+    // console.log(idText);
     document.querySelector(".login-form").style.display = "none";
     document.querySelector(".login-text").style.display = "none";
     document.querySelector("#result-id").innerHTML = document.form1.id.value;
+    document.querySelector("#result-email").innerHTML = Cookies.get('email');
     document.querySelector(".result").setAttribute("style","display: block;" );
   }
   else if (state == false) {
     return;
   }
-  else {
-    alert("아이디와 비밀번호를 정확하게 입력해 주세요");
+  else if (idText != Cookies.get('id')){
+    alert("가입한 아이디와 일치하지 않습니다.");
+  }
+  else if (pwText != Cookies.get('password')){
+    alert("가입한 비밀번호와 일치하지 않습니다.");
   }
 
+
 }
+
+    
